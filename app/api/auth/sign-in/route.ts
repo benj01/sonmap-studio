6ee9869d0ebb09a2d19f42c1993312db3d9b7e78
benj01 <benjamin.hem@gmail.com>
2024-12-07 14:30:19 +0100

    b2

---
+++ app/api/auth/sign-in/route.ts
@@ -0,0 +1,27 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url)
+  const formData = await request.json()
+  const cookieStore = cookies()
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: formData.email,
+    password: formData.password,
+  })
+
+  if (error) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 400 }
+    )
+  }
+
+  return NextResponse.json(
+    { user: data.user },
+    { status: 200 }
+  )
+} 
