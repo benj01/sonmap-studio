9a65b032bfaacd7aa4726315f2b3fdf4f323e90d
benj01 <benjamin.hem@gmail.com>
2024-12-07 16:16:21 +0100

    b5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
-import { AuthProvider } from '@/components/providers/auth-provider'
-import { ModalProvider } from '@/components/providers/modal-provider'
-import { Header } from '@/components/layout/header'
-import { Footer } from '@/components/layout/footer'
-import { Toaster } from '@/components/ui'
+import { GeistSans } from 'geist/font/sans'
+import { ThemeProvider } from '../components/providers/theme-provider'
+import { AuthProvider } from '../components/providers/auth-provider'
+import { ModalProvider } from '../components/providers/modal-provider'
+import { Header } from '../components/layout/header'
+import { Footer } from '../components/layout/footer'
+import { Toaster } from '../components/ui'
+import './globals.css'
 
 export default function RootLayout({
   children,
@@ -10,19 +13,26 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <AuthProvider>
-          <div className="relative flex min-h-screen flex-col">
-            <Header />
-            <main className="flex-1">
-              {children}
-            </main>
-            <Footer />
-            <ModalProvider />
-            <Toaster />
-          </div>
-        </AuthProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={GeistSans.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            <div className="relative flex min-h-screen flex-col">
+              <Header />
+              <main className="flex-1">
+                {children}
+              </main>
+              <Footer />
+              <ModalProvider />
+              <Toaster />
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
